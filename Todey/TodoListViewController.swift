20c018b84e3addba88e5909518e09b6e730ba87a
Karan Jivai <karankumar.jivani@united.com>
2018-04-07 21:13:08 -0500

    Added functionality to persist ToDo list data using NSUserDefauls

--- Todey/TodoListViewController.swift
+++ Todey/TodoListViewController.swift
@@ -12,12 +12,16 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Vegetables", "Milk", "Chips"]
     
+    let defaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK:- Tabelview Datasource Methods
@@ -64,6 +68,9 @@ class TodoListViewController: UITableViewController {
                 // string contains non-whitespace characters
                 
                 self.itemArray.append(textField.text!)
+                
+                self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+                
                 self.tableView.reloadData()
             }
             
